530931ebf50d701a85e682ba48cb03d31d53a248
pumpkinsuu <pumpkinsuu@gmail.com>
2021-04-13 18:48:20 +0700

    update

--- face/face.py
+++ face/face.py
@@ -81,19 +81,19 @@ def create_face_bp(app, model):
             if v:
                 return v
 
-            if 'front' not in request.form:
+            if 'front' not in request.json:
                 return res_cors({
                     'status': 400,
                     'message': 'missing "front"',
-                    'data': request.form
+                    'data': request.json
                 }), 400
-            if 'left' not in request.form:
+            if 'left' not in request.json:
                 return res_cors({
                     'status': 400,
                     'message': 'missing "left"',
                     'data': ''
                 }), 400
-            if 'right' not in request.form:
+            if 'right' not in request.json:
                 return res_cors({
                     'status': 400,
                     'message': 'missing "right"',
@@ -107,9 +107,9 @@ def create_face_bp(app, model):
                     'data': ''
                 }), 404
 
-            front = load_img(request.form['front'])
-            left = load_img(request.form['left'])
-            right = load_img(request.form['right'])
+            front = load_img(request.json['front'])
+            left = load_img(request.json['left'])
+            right = load_img(request.json['right'])
 
             code, result = face_api.get_user(username)
 
@@ -119,9 +119,9 @@ def create_face_bp(app, model):
                 if code == 201:
                     code, result = photo_api.create_user(
                         username,
-                        left=request.form['left'],
-                        right=request.form['right'],
-                        front=request.form['front']
+                        left=request.json['left'],
+                        right=request.json['right'],
+                        front=request.json['front']
                     )
                     if code != 201:
                         face_api.remove_user(username)
@@ -131,9 +131,9 @@ def create_face_bp(app, model):
                 if code == 200:
                     code, result = photo_api.update_user(
                         username,
-                        left=request.form['left'],
-                        right=request.form['right'],
-                        front=request.form['front']
+                        left=request.json['left'],
+                        right=request.json['right'],
+                        front=request.json['front']
                     )
 
             return res_cors({
